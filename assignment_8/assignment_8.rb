aa167ba2944f6392ae7e5ad44fd535b6dab8d227
Michael Scriven <bigscriv03@gmail.com>
2014-09-24 22:40:39 -0400

    Submitting with errors #13

--- assignment_8/assignment_8.rb
+++ assignment_8/assignment_8.rb
@@ -1,19 +1,16 @@
-#!/usr/bin/ruby
-
-# If user enter name, wish them a Happy Birthday....
-
+require 'date'
 
 if ARGV.size == 0
 print "What is your name?"
 name = $stdin.gets.chomp
+print "What is your birthday?"
+birthday = gets.chomp
+Date.civil("#{birthday}").parse
+
+puts birthday.strftime('%b/%d/%Y')
+
 puts "Happy Birthday #{name}."
 
 else
 puts "Happy Birthday #{ARGV[0]}."
 end
-
-
-
-=begin
-this is a comment
-=end
