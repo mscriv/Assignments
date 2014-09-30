1507b6255b3375972550c699fd516c8ba022b91c
Michael Scriven <bigscriv03@gmail.com>
2014-09-30 17:04:32 -0400

    Corrected counter in Guessing game, #29

--- assignment_13/solution_13.rb
+++ assignment_13/solution_13.rb
@@ -1,26 +1,27 @@
-count = 1
 
-num  = rand(1..3)
-
-loop do
-
-print "Enter a number 1 to 3 to play:\n"
-guess = gets.chomp.to_i
-
-puts "Your guess was #{guess}."
+    count = 0
 
 
-if guess > num
-    puts 'Your guess was too high'
-elsif guess < num
-    puts 'Your guess was too low'
-  else
-#elsif guess == num
-    puts 'Good guess, Next stop Vegas!'
-   break
-end
-end
+    num  = rand(1..3)
 
-print "The total number of guesses is:\n"
+    loop do
 
-puts count
+
+        print "Enter a number 1 to 3 to play:\n"
+        guess = gets.chomp.to_i
+
+        puts "Your guess was #{guess}."
+        count +=1
+
+    if guess > num
+        puts 'Your guess was too high'
+    elsif guess < num
+        puts 'Your guess was too low'
+      else
+    #elsif guess == num
+        puts 'Good guess, Next stop Vegas!'
+        print "The total number of guesses is:\n"
+        puts count 
+      break
+    end
+  end
