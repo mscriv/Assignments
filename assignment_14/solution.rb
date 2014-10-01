dc1bfdd1e138b5c0cfc92dc049d546d4de8cfda4
Michael Scriven <bigscriv03@gmail.com>
2014-10-01 17:36:43 -0400

    completed assignment_14, #31

--- assignment_14/solution.rb
+++ assignment_14/solution.rb
@@ -1,25 +1,25 @@
 
 
-  def pigatize(text)
-    if starts_with_vowel(text)
-      pigatized_text = text
-  else
-       pigatized_text = text
-  end
-      return pigatized_text
-end
+      def pigatize(text)
+        if starts_with_vowel(text) == true
+           pigatized_text = text + "way"
 
-def starts_with_vowel(text)
-    if /[aeiou]/.match("#{text[0]}")
-    return true
-    else
-    return false
+        else
+           a = text.chars.rotate.join
+           pigatized_text = a + "ay"
+      end
+         return pigatized_text
     end
-end
 
-loop do
-    puts "Please enter a word and I will translate to pig latin"
-    text = gets.chomp
-  break if text.length == 0
-    #puts pigatize(text)
-end
+
+    def starts_with_vowel(text)
+        return
+          /[aeiou]/.match(text[0])
+    end
+
+    loop do
+        puts "Please enter a word and I will translate to pig latin"
+        text = gets.chomp
+      break if text.length == 0
+        puts pigatize(text)
+    end
