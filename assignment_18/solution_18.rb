76b4143426b67b9b585254fb57a2e7fe5c244c60
Michael Scriven <bigscriv03@gmail.com>
2014-10-06 17:05:38 -0400

    Added URI escape to title params, #36

--- assignment_18/solution_18.rb
+++ assignment_18/solution_18.rb
@@ -2,27 +2,25 @@
     require 'uri'
     require 'json'
 
+
     print "Please enter a movie title:\n"
 
     title = $stdin.gets.chomp
 
 
-    parms = "title=#{title}"
+    parms = URI.escape("title=#{title}")
     parms = parms + "&format=JSON"
-    # Other parms
-    #parms = parms + "&plot"
 
     uri = URI('http://www.myapifilms.com/imdb?' + parms)
 
     response = Net::HTTP.get_response(uri)
 
     data = response.body
-    #puts data
+
     result = JSON.parse(data).first
     s = result["plot"]
-    puts s
+      puts s
 
     open("movie_script.csv", "a") do |f|
       f.puts s
     end
-    
