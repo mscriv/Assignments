a0cd3d0d200a0703970f9a3c335348de7c19a8b2
Michael Scriven <bigscriv03@gmail.com>
2014-10-14 17:55:25 -0400

    Assignment 25 unable to call delete employee

--- assignment_25/assignment_25.rb
+++ assignment_25/assignment_25.rb
@@ -1,44 +1,60 @@
-  class Employee
-      attr_accessor :name, :city
+      class Employee
 
-      def initialize(employee)
-        @name = name
-        @city = city
-      end
-    end
+            @@employee_num = 0
+            attr_accessor :name, :city, :id_num
 
-    class Company
-
-      attr_accessor :company_name, :company_city, :employees
-
-      def initialize(employees)
-        @employees = []
+            def initialize
+              @@employee_num += 1
+              @id_num = @@employee_num
+            end
       end
 
+      class Company
 
-      def hire_employee
-          print "enter your name:\n"
-          name = gets.chomp
-          employees.push("#{name}")
-          print "enter your city:\n"
-          @city = gets.chomp
-          puts "congrats you've been hired"
+            attr_accessor :company_name, :company_city, :employees
+
+            def initialize
+              @employees = []
+            end
+
+
+            def hire_employee(employee)
+              @employees << employee
+            end
+
+
+            def fire_employee(employee)
+               @employees.reject! { |employee| employee.id_num == employee.id_num }
+            end
+
       end
 
+              c = Company.new
+              print "Enter the company name:\n"
+              c.company_name = gets.chomp.strip
+              print "Enter the company's location (city):\n "
+              c.company_city = gets.chomp.strip
 
-      def fire_employee
-          print "enter a name to delete:\n"
-          @name = gets.chomp
-          employees.match(/@name/).pop
-          puts "You no longer work here"
-      end
+            loop do
+              employee = Employee.new
+              print "Enter the employee name:\n"
+              employee.name = gets.strip
+              break if employee.name == ""
+              print "Enter the employees city:\n"
+              employee.city = gets.strip
+              c.hire_employee(employee)
+            end
 
-      def company
-        @company = company_name
-        @company_city = company_city
-      end
-    end
+             c.employees.each do |employee|
+               puts "#{employee.name}, #{employee.city}, #{employee.id_num}"
+             end
 
-     e = Company.new('employees')
-     e.hire_employee
-     puts e.employees.count
+             print "Enter an employee number to terminate:\n"
+             answer = gets.to_i
+             term_employee = c.employees.select { |employee| employee.id_num == "#{answer}" }
+
+             c.fire_employee(term_employee)
+
+            c.employees.each do |employee|
+              puts "#{employee.name}, #{employee.city}, #{employee.id_num}"
+            end
